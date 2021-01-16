bb2ca132a615e89deda31bb5b589e86f04317bfe
balvantghanekar11 <balvantghanekar03@gmail.com>
2021-01-17 01:37:41 +0530

    add user and routes

--- surf-shop/app.js
+++ surf-shop/app.js
@@ -7,7 +7,6 @@ const logger = require("morgan");
 const indexRouter = require("./routes/index");
 const postRouter = require("./routes/posts");
 const reviewRouter = require("./routes/reviews");
-const usersRouter = require("./routes/users");
 
 const app = express();
 
@@ -24,7 +23,6 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/posts", postRouter);
 app.use("/posts/:id/reviews", reviewRouter);
-app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- surf-shop/routes/index.js
+++ surf-shop/routes/index.js
@@ -6,4 +6,49 @@ router.get("/", (req, res, next) => {
   res.render("index", { title: "Surf Shop | Home" });
 });
 
+/* POST /register */
+router.post("/register", (req, res, next) => {
+  res.send("POST /register");
+});
+
+/* GET /login */
+router.get("/login", (req, res, next) => {
+  res.send("GET /login");
+});
+
+/* POST /login */
+router.post("/login", (req, res, next) => {
+  res.send("POST /login");
+});
+
+/* GET /profile */
+router.get("/profile", (req, res, next) => {
+  res.send("GET /profile");
+});
+
+/* PUT /profile/:user_id */
+router.put("/profile/:user_id", (req, res, next) => {
+  res.send("PUT /profile/:user_id ");
+});
+
+/* GET /forgot-password */
+router.get("/forgot-password", (req, res, next) => {
+  res.send("GET /forgot-password");
+});
+
+/* PUT /forgot-password */
+router.put("/forgot-password", (req, res, next) => {
+  res.send("PUT /forgot-password");
+});
+
+/* GET /reset-password */
+router.get("/reset-password/:token", (req, res, next) => {
+  res.send("GET /reset-password/:token");
+});
+
+/* PUT /reset-password */
+router.put("/reset-password/:token", (req, res, next) => {
+  res.send("PUT /reset-password/:token");
+});
+
 module.exports = router;
